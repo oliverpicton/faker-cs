37d4647883f6dac5ce275725fd3ee5022fab34e4
Oliver Picton <opicton@gmail.com>
2016-04-28 12:50:27 +0100

    Added commerce class in for generated E-Commerce related strings. This functiuonality is a port of what is in the original Ruby library.

---
+++ src/Faker/Commerce.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Faker.Extensions;
+
+namespace Faker
+{
+    public class Commerce
+    {
+        public static string ProductName()
+        {
+            return Resources.Commerce.productNameAdjective.Split(Config.Separator).Random() + ' ' + Resources.Commerce.productNameMaterial.Split(Config.Separator).Random() + ' ' + Resources.Commerce.productName.Split(Config.Separator).Random();
+        }
+
+        public static string Department()
+        {
+            return Resources.Commerce.department.Split(Config.Separator).Random();
+        }
+
+        public static string Price()
+        {
+            return "##.##".Numerify();
+        }
+    }
+}
---
+++ tests/Faker.Tests/CommerceFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Faker.Tests
+{
+    [TestFixture]
+    public class CommerceFixture
+    {
+        [Test]
+        public void Should_Get_ProductName()
+        {
+            var product = Commerce.ProductName();
+            Console.WriteLine(product);
+            Assert.IsNotEmpty(product);
+        }
+
+        [Test]
+        public void Should_Get_Price()
+        {
+            var price = Commerce.Price();
+            Console.WriteLine(price);
+            Assert.IsNotEmpty(price);            
+        }
+
+        [Test]
+        public void Should_Get_Department()
+        {
+            var department = Commerce.Department();
+            Console.WriteLine(department);
+
+            Assert.IsNotEmpty(department);
+        }
+    }
+}
